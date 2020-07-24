19967ceae72fe109830024615b9fb2c6f82fa562
Dinesh Silva <dinesh_silva@dialog.com.au>
2020-07-24 10:53:50 +1000

    Added a reminder to clean up code

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -16,7 +16,7 @@ namespace CommandAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetCommandItems()
         {
-            // return new string[] {"this", "is", "hard", "coded"};
+            // return new string[] {"this", "is", "hard", "coded", "Array"};
             return _context.CommandItems;
         }
 
